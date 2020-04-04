749c363dd5e91a43150007cede7b51f05760cdb8
liangrongjun <liangrongjun@saicmobility.com>
2020-04-04 16:09:42 +0800

    路由拆分

--- app/middleware/filter.js
+++ app/middleware/filter.js
@@ -1,7 +1,7 @@
 // 过滤
 module.exports = (options, app) => {
   return async function filter(ctx, next) {
-    if(ctx.get("user-agent").indexOf('Chrome') !== -1) {
+    if(ctx.get("user-agent").indexOf('node') !== -1) {
       ctx.redirect(options.redirectUrl);
     } else {
       await next();
--- app/router.js
+++ app/router.js
@@ -1,17 +1,5 @@
 module.exports = (app) => {
   const { router, controller } = app;
-
-  router.get("homeIndex", '/index', controller.home.index.index);  // txt,ts上传失败
-  router.post('/upload', controller.home.index.upload);
-  
-  router.get('/curl', controller.home.index.curl); 
-
-  router.get('/middleware', controller.home.index.middleware); // 中间件
-  
-  router.get('newslist', '/getNews', controller.home.index.getNews);
-
-  router.get('/handleCookie', controller.home.index.handleCookie);  // 设置cookie
-  // router.get('/', controller.home.index.index);  // 文件上传
-  router.get('adminIndex', '/admin', controller.admin.index.index);  // 第一个参数是路由名称
-  router.post('postData', '/postData', controller.admin.index.postData);
+  require('./router/home')(app);
+  require('./router/admin')(app);
 }
